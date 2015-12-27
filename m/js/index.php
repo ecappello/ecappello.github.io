9e0d12a14ebcaa0eb71df1c3127424a78773bdf9
Eric Cappello <ecappello@Macintosh.local>
2015-12-26 20:29:15 -0800

    Initial commit

---
+++ m/js/index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>CLASSROOM AQUATIC</title>
+	<link rel="stylesheet" type="text/css" href ="css/reset.css">
+	<link rel="stylesheet" type="text/css" href ="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+	</head>
+	
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-28885124-3', 'classroomaquatic.com');
+		ga('send', 'pageview');
+	</script>
+
+	<script>
+
+	$(document).ready(function(){
+
+		$('.navGame').click(function () {
+	    	$('html, body').animate({
+			scrollTop: $('#content').offset().top}, '1000');
+
+		});
+
+		$('.navTeam').click(function () {
+	    	$('html, body').animate({
+			scrollTop: $('#team').offset().top}, '1000');
+
+		});
+
+		$('.navDownload').click(function () {
+	    	$('html, body').animate({
+			scrollTop: $('#downloads').offset().top}, '1000');
+
+		});
+
+		$('.navContact').click(function () {
+	    	$('html, body').animate({
+			scrollTop: $('#contact').offset().top}, '1000');
+
+		});
+
+
+
+	});
+
+
+	</script>
+
+
+
+	<body>
+
+	
+		<div id="wrapper">
+
+			<div id="logo">
+
+			</div>
+		
+			<div id="content">
+
+				<table margin="auto" style="margin-top:-30px;margin-bottom:20px">
+				<tr>
+					<td>
+						<a style="margin-left:105px;" href="http://steamcommunity.com/sharedfiles/filedetails/?id=227768957" target="_blank">
+						<img class="banners" src="/images/ca_greenlight_small.png"></a>
+					</td>
+					<td>
+						<a style="margin-left:20px;" href="https://www.kickstarter.com/projects/2094628033/classroom-aquatic" target="_blank">
+						<img class="banners" src="/images/ca_kickstarter_small.png"></a>
+					</td>
+				</tr>
+				</table>
+				
+				<div id="intro">
+				Classroom Aquatic is the world's first cheat-'em-up. Players play as an exchange student in a school of dolphins, going through a semester that 
+				they're in no way prepared for. 
+				They must resort to cheating in order to become the best student in school! 
+				</div>
+			<div class= "border"></div>
+				
+			<div id ="trailer">
+				
+				<iframe width="704" height="450" src="//www.youtube.com/embed/GidFsoVK8I0?rel=0" frameborder="0" allowfullscreen></iframe>
+		
+
+			</div>
+			<br>
+			<!-- <br>
+			 <h1><a href="https://www.kickstarter.com/projects/2094628033/classroom-aquatic">KICKSTARTER</a></h1>
+			<p>
+			Like what you see? Want more Classroom Aquatic? Check out our kickstarter <a href="https://www.kickstarter.com/projects/2094628033/classroom-aquatic">here</a>.
+			</p> -->
+
+			<div id="downloads">
+
+				<h1>DOWNLOAD</h1>
+
+					<p style="margin-bottom:40px;">
+					Note that there will be bugs as the game is still in ALPHA. <a href = "http://www.classroomaquatic.com/support/">Please report all bugs to our support page.</a><br>
+					Torrent of the demo can be downloaded <a href="http://www.classroomaquatic.com/downloads/ClassroomAquatic_3-1.torrent">here</a><br>
+					We recommend uTorrent for your bitTorrent needs. Download it <a href="http://www.utorrent.com/downloads/win">here</a>
+<!--					NOTICE: Due to a server malfunction the downloads are not availble at this time. Please check back later. -->
+					</p>
+			<!--
+				<div id="downloadcolumn">
+					<div class="osIcons"><img src="/images/icon_windows.png"/></div>
+
+					<ul id="downloadList">
+					<li>
+						<h3>WIN Build v2.1 - Oculus</h4>
+						Xbox Gamepad: <a href="/downloads/v10/2-11_Win_Oculus.zip" target="_blank" class="dl" onClick="ga('send', 'event', 'WIN', 'Download', 'OculusGamepad');">download</a>
+					</li>
+
+					<li>
+						<h3>WIN Build v2.1 - No Oculus</h4>
+						Keyboard: <a href="/downloads/v10/2-11_Win_Single.zip" target="_blank" class="dl" onClick="ga('send', 'event', 'WIN', 'Download', 'SingleKeyboard');">download</a>
+					</li>
+				</div>
+
+				<div id="downloadcolumn">
+					<div class="osIcons"><img src="/images/icon_mac.png"/></div>
+
+					<ul id="downloadList">
+					<li>
+						<h3>MAC Build v2.1 - Oculus</h4>
+						Xbox Gamepad: <a href="/downloads/v10/2-11_Mac_Oculus.zip" target="_blank" class="dl" onClick="ga('send', 'event', 'MAC', 'Download', 'OculusGamepad');">download</a>
+					</li>
+
+					<li>
+						<h3>MAC Build v2.1 - No Oculus</h4>
+						Keyboard: <a href="/downloads/v10/2-11_Mac_Single.zip" target="_blank" class="dl" onClick="ga('send', 'event', 'MAC', 'Download', 'SingleKeyboard');">download</a>
+					</li>
+				</div>
+
+				<div id="downloadcolumn">
+					<div class="osIcons"><img src="/images/icons_linux.png"/></div>
+
+					<ul id="downloadList">
+					<li>
+						<h3>LINUX Build v2.1 - Oculus</h4>
+						Xbox Gamepad: <a href="/downloads/v10/2-11_Linux_Oculus.zip" target="_blank" class="dl" onClick="ga('send', 'event', 'LINUX', 'Download', 'OculusGamepad');">download</a>
+					</li>
+
+					<li>
+						<h3>LINUX Build v2.1 - No Oculus</h4>
+							Keyboard: <a href="/downloads/v10/2-11_Linux_Single.zip" target="_blank" class="dl" onClick="ga('send', 'event', 'LINUX', 'Download', 'SingleKeyboard');">download</a>
+					</li>
+				</div>
+			
+				-->
+			</div>
+			
+
+			<div id="team">
+			<div style="position:absolute; margin-top: -60px;margin-left:-140px; opacity:0.15;"><img src="/images/equations.png"/img></div>
+
+
+			<h1>THE TEAM</h1>
+
+			<div class ="colLeft">
+
+				<ul class = "teamList">
+
+					<li>
+						<h4>ERIC CAPPELLO // MUSIC & SOUND DESIGN</h4>
+						<p>
+						Eric Cappello is a young, award-winning film composer whose body of work also includes numerous concert music pieces, 
+						orchestral and chamber works, as well as multi-genre songs and ambient, electronic music. <a href="http://www.ericcappello.com/">http://www.ericcappello.com/</a>
+						</p>
+
+					</li>
+
+					<li>
+
+						<h4>MICKEY GOESE // CHARACTER ANIMATION</h4>
+						<p>
+						Mickey Goese is a self-taught 3D artist and loves polygons.
+						<a href="http://www.mickeygoese.com/">http://www.mickeygoese.com/</a>
+						</p>
+
+					</li>
+					<li>
+
+						<h4>HEATHER PENN // ENVIRONMENT ART</h4>
+						<p>
+						Heather Penn thinks that the ocean is really great. <a href="http://penn.pithytwigs.com/">http://penn.pithytwigs.com/</a>
+						</p>
+						
+					</li>
+			</div>
+
+			<div class ="colRight">
+
+				<ul class = "teamList">
+
+					<li>
+						<h4>ADELINE 'AMMONITE' DUCKER // ENVIRONMENT ART</h4>
+						<p>
+						Ammonite always wanted to be a sea monster when she grew up.
+						<br>
+						<a href="http://cargocollective.com/adelineducker/">http://cargocollective.com/adelineducker/</a>	
+						</p>
+						
+					</li>
+
+						<li>
+						
+						<h4>REMY KARNS // PRODUCTION & WRITING</h4>
+						<p>
+						Remy Karns is a graduate student studying Interactive Media and Games at USC. 
+						He doesn't know that a dolphin isn't a fish. Don't correct him-- we find his ignorance amusing.
+						</p>
+
+					</li>
+
+					<li>
+
+						<h4>MIKAL SALTVEIT // PROGRAMMING</h4>
+						<p>
+						Mikal Saltveit is a programmer who dreamed one day of making the perfect game. That game turned out to be a dolphin cheating simulator.
+						<br>
+						<a href="www.homebrood.info">www.homebrood.info/</a>
+						</p>	
+
+					</li>	
+			
+				</ul>
+	
+			</div>
+				
+			</div>
+
+			<div id="contact">
+
+				<div style="position:absolute;;margin-left:-140px; opacity:0.25; z-index: -100;"><img src="/images/fishes.png"/img></div>
+			
+
+				<ul class="icons">
+					<li><a href="mailto:contact@classroomaquatic.com"><img src="/images/icon_mail.png"></a></li>
+					<li><a href="https://twitter.com/ClassAquatic"><img src="/images/icon_twitter.png"></a></li>
+					<li><a href="https://www.facebook.com/ClassroomAquatic"><img src="/images/icon_facebook.png"></a></li>
+				</ul>
+
+				<p>
+				Email us via <font color="#f68a73">contact@classroomaquatic.com</font>
+	
+				if you have any comments or questions.
+			</p>
+			
+			<!--	<p>Download our press kit <a href="/downloads/PressKit.pdf" target="_blank" class="dl" onClick="ga('send', 'event', 'PDFS', 'Download', 'PressKit');">here</a>.</p> -->
+
+				<p>Like us on <a href="https://www.facebook.com/ClassroomAquatic">Facebook</a>.</p>
+
+				<p>Follow us on <a href="https://twitter.com/ClassAquatic">Twitter</a>.</p>
+
+				<p>Listen to the <a href="https://www.soundcloud.com/ericcappello/sets/classroom-aquatic-soundtrack" target="_blank" 		onclick="MM_openBrWindow('https://www.soundcloud.com/ericcappello/sets/classroom-aquatic-soundtrack','Soundtrack','scrollbars=yes,width=550,height=320'); return false;">Original Soundtrack</a>.</p>
+				
+				<!--
+					<a id="foxyform_embed_link_473306" href="http://www.foxyform.com/"></a>
+				<script type="text/javascript">
+					(function(d, t){
+					var g = d.createElement(t),
+					s = d.getElementsByTagName(t)[0];
+					g.src = "http://www.foxyform.com/js.php?id=473306&sec_hash=a7f6b896517&width=350px";
+					s.parentNode.insertBefore(g, s);
+					}(document, "script"));
+				</script>
+				-->
+	
+				
+			</div>
+
+
+
+			
+		</div>
+
+		
+	</body>
+
+</html>
---
+++ m/js/playlist.js
@@ -0,0 +1,196 @@
+//<![ playlist [
+
+$(document).ready(function(){
+
+	new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_1",
+		cssSelectorAncestor: "#jp_container_1"
+	}, [
+		
+		{
+			title:"Chasing Shadows",
+			//artist:"&nbsp;Score Excerpt",
+			oga:"http://m.ericcappello.com/music/Halloween_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Halloween_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Halloween_web.mp3",
+			poster: "http://m.ericcappello.com/images/Halloween_cover.jpg"
+		},
+		{
+			title:"\"HOZON\" Trailer",
+			artist:"- &nbsp;Score Excerpt",
+			oga:"http://m.ericcappello.com/music/Sci-Fi_Demo_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Sci-Fi_Demo_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Sci-Fi_Demo_web.mp3",
+			poster: "http://m.ericcappello.com/images/HOZON_teaser_cover.png"
+		},
+		{
+			title:"Doctor of Bagram",
+			artist:"- Trauma Scene",
+			webmv: "http://www.ericcappello.com/music/DOB_Trauma.webm",
+			m4v:"http://www.ericcappello.com/music/DOB_Trauma.m4v",
+			//ogv:"http://www.ericcappello.com/music/DOB_Trauma.ogv",
+			//poster:"http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png"//
+		},
+		{
+			title:"Across The Room",
+			artist:"- (Electro Song)",
+			oga:"http://m.ericcappello.com/music/Across_the_room_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Across_the_room_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Across_the_room_web.mp3",
+			poster: "http://m.ericcappello.com/images/Across_the_room_cover.jpg"
+		},
+		{
+			title:"Opening",
+			artist:"- \"Pani's Box\" Score Excerpt",
+			oga:"http://m.ericcappello.com/music/Pani's_box_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Pani's_box_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Pani's_box_web.mp3",
+			poster: "http://m.ericcappello.com/images/Pani's_box_dark_cover.png"
+		},
+		{
+			title:"Cryptoid Blues",
+			artist:"- Game Music",
+			oga:"http://m.ericcappello.com/music/Cryptoid_blues_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Cryptoid_blues_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Cryptoid_blues_web.mp3",
+			poster: "http://m.ericcappello.com/images/Cryptoid_blues_cover.png"
+		},
+		{
+			title:"Waited So Long",
+			artist:"- \"Llegar A Ti\" Score Excerpt",
+			oga:"http://m.ericcappello.com/music/Llegar_end_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Llegar_end_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Llegar_end_web.mp3",			
+			poster: "http://m.ericcappello.com/images/New Llegar photo.png"
+		},
+		{
+			title:"Foxglove",
+			artist:"- Flashback Scene",
+			webmv: "http://www.ericcappello.com/music/FG_Flashback_Web.webm",
+			m4v:"http://www.ericcappello.com/music/FG_Flashback_Web.m4v",
+			//ogv:"http://www.ericcappello.com/music/FG_Flashback_Web.ogv",
+			//poster:"http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png"
+		},
+		{
+			title:"\"HOZON\"",
+			artist:"&nbsp;Creepy Teaser Score",
+			oga:"http://m.ericcappello.com/music/HOZON_creepy_web.ogg",
+			m4a:"http://m.ericcappello.com/music/HOZON_creepy_web.m4a",
+			mp3:"http://m.ericcappello.com/music/HOZON_creepy_web.mp3",			
+			poster: "http://m.ericcappello.com/images/HOZON_science_cover.png"
+		},
+		{
+			title:"Otis Commercial",
+			artist:"",
+			oga:"http://m.ericcappello.com/music/Otis_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Otis_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Otis_web.mp3",
+			poster: "http://m.ericcappello.com/images/Otis_cover.png"
+		},
+		{
+			title:"Forest Of Dreams",
+			artist:"- for Orchestra",
+			oga:"http://m.ericcappello.com/music/Forest_of_Dreams.ogg",
+			m4a:"http://m.ericcappello.com/music/Forest_of_Dreams.m4a",
+			mp3:"http://m.ericcappello.com/music/Forest_of_Dreams.mp3",
+			poster: "http://m.ericcappello.com/images/Forest_of_dreams_cover.jpg"
+		},
+		{
+			title:"Upstream In Autumn",
+			artist:"- for Piano Quintet",
+			oga:"http://m.ericcappello.com/music/Upstream_In_Autumn.ogg",
+			m4a:"http://m.ericcappello.com/music/Upstream_In_Autumn.m4a",
+			mp3:"http://m.ericcappello.com/music/Upstream_In_Autumn.mp3",
+			poster: "http://m.ericcappello.com/images/Upstream_in_autumn_cover.png"
+		},
+		{
+			title:"\"Late Night Snack\"",
+			artist:"&nbsp;Original Score",
+			oga:"http://m.ericcappello.com/music/Late_night_snack_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Late_night_snack_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Late_night_snack_web.mp3",
+			poster: "http://m.ericcappello.com/images/Late_Night_Snack_cover.png"
+		},
+		{
+			title:"\"Renegade\"",
+			artist:"&nbsp;Score Excerpt",
+			oga:"http://m.ericcappello.com/music/Renegade_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Renegade_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Renegade_web.mp3",
+			poster: "http://m.ericcappello.com/images/Renegade_cover.jpg"
+		},
+		{
+			title:"\"Fallen\"",
+			artist:"&nbsp;Original Score",
+			oga:"http://m.ericcappello.com/music/Fallen_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Fallen_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Fallen_web.mp3",
+			poster: "http://m.ericcappello.com/images/Fallen_cover2.png"
+		},
+		{
+			title:"Cosmic Chrysalis",
+			artist:"",
+			oga:"http://m.ericcappello.com/music/Cosmic_chrysalis_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Cosmic_chrysalis_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Cosmic_chrysalis_web.mp3",
+			poster: "http://m.ericcappello.com/images/Cosmic_chrysalis_cover.jpg"
+		},
+		{
+			title:"Wandering Nights",
+			artist:"- for Sax, Guitar, Violin",
+			oga:"http://m.ericcappello.com/music/Wandering_nights_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Wandering_nights_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Wandering_nights_web.mp3",
+			poster: "http://m.ericcappello.com/images/Wandering_nights_cover.png"
+		},
+		{
+			title:"The Pool",
+			artist:"- \"Llegar A Ti\" Score Excerpt",
+			oga:"http://m.ericcappello.com/music/Llegar_pool_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Llegar_pool_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Llegar_pool_web.mp3",
+			poster: "http://m.ericcappello.com/images/Llegar_pool_cover.png"
+		},
+		{
+			title:"\"The Gladness Of Nature\"",
+			artist:"- &nbsp;Text Setting",
+			oga:"http://m.ericcappello.com/music/Gladness_nature_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Gladness_nature_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Gladness_nature_web.mp3",
+			poster: "http://m.ericcappello.com/images/Gladness_nature_cover.jpg"
+		},
+		{
+			title:"\"Soundworld\"",
+			artist:"- &nbsp;Excerpt",
+			oga:"http://m.ericcappello.com/music/Soundworld_web.ogg",
+			m4a:"http://m.ericcappello.com/music/Soundworld_web.m4a",
+			mp3:"http://m.ericcappello.com/music/Soundworld_web.mp3",
+			poster: "http://m.ericcappello.com/images/Soundworld_cover.jpg"
+		},
+		{
+			title:"\"Twice In A Lifetime\"",
+			artist:"- &nbsp;Score Excerpt",
+			oga:"http://m.ericcappello.com/music/TIAL_web.ogg",
+			m4a:"http://m.ericcappello.com/music/TIAL_web.m4a",
+			mp3:"http://m.ericcappello.com/music/TIAL_web.mp3",
+			poster: "http://m.ericcappello.com/images/TIAL_cover.png"
+		},
+		
+	], {
+		
+		playlistOptions: {
+		    autoPlay: true,
+			
+		  },
+		
+		swfPath: "js",
+		supplied: "webmv, ogv, m4v, oga, m4a, mp3",
+		smoothPlayBar: true,
+		volume: 0.99,
+		keyEnabled: true,
+		preload: "auto",
+		audioFullScreen: false
+	});
+});
+
+//]]>
